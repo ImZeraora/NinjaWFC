8146e9862331b84851d19b2fe828c9d54b23e0e7
TheNinjaKingOW <54253237+TheNinjaKingOW@users.noreply.github.com>
2021-05-19 13:34:54 -0400

    Add files via upload

--- error.js
+++ error.js
@@ -88,7 +88,7 @@ function error(){
         document.getElementById("result").innerHTML=error + ": " + "The gamestats server is not implemented. If you have well-documented packet captures from the official WFC, you should file an issue with them and perhaps a volunteer programmer can implement it. For the time being, the game is unsupported."
     }
     else if(error=="trolled"){
-        location.replace("45.63.14.99/troll.mp4")
+        location.replace("http://www.ninjawfc.com/troll.mp4")
     }
     else{
         document.getElementById("result").innerHTML="This error does not exist." 
