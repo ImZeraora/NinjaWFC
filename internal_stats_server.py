166a9f0d8be4cd330f2a19441aa36be1998d30e4
TheNinjaKingOW <54253237+TheNinjaKingOW@users.noreply.github.com>
2021-06-01 14:51:11 -0400

    Add files via upload

--- internal_stats_server.py
+++ internal_stats_server.py
@@ -381,12 +381,7 @@ function time() {
         document.getElementById("monthimg").src="";
 
         document.getElementById("header").style.backgroundColor = "RGB(56,20,96)";
-
-        if (page == "index.html") {
-            document.getElementById("newsp").innerHTML = "If you are reading this, it means that the website has worked and you can now connect to NinjaWFC! The website will be \"completed\" by the end of June and will have features like, Being able to detect how many players are on the server with more advanced methods, interactive design, and possibly mobile support (more to come).";
-        }
-        else {
-        }
+        
         document.getElementById("NinjaWFCTitle").innerHTML = "NinjaWFC";
 
         document.getElementById("logo-home").src = "https://i.ibb.co/GMK2Zst/logo.png";
