b7a6262510147ace6ed2da438a924eb2663c3dc0
Zeraora <54253237+ZeraoraKing@users.noreply.github.com>
2024-06-29 21:08:00 -0400

    Update time.js

--- time.js
+++ time.js
@@ -45,27 +45,6 @@ window.onload = function () {
     });
 }
 
-function defaulttheme(){
-    eventclear = 1;
-    document.getElementById("monthimg").src="";
-
-        document.getElementById("header").style.backgroundColor = "RGB(56,20,96)";
-        document.getElementById("body").style.backgroundColor = "white";
-        document.getElementById("body").style.color = "black";
-
-        if (page == "index.html") {
-            document.getElementById("newsp").innerHTML = "If you are reading this, it means that the website has worked and you can now connect to NinjaWFC! The website will be \"completed\" by the end of June and will have features like, Being able to detect how many players are on the server with more advanced methods, interactive design, and possibly mobile support (more to come).";
-        }
-        else {
-        }
-        document.getElementById("NinjaWFCTitle").innerHTML = "NinjaWFC";
-
-        document.getElementById("logo-home").src = "https://i.ibb.co/GMK2Zst/logo.png";
-
-        document.getElementById("monthimg").style.display="none";
-}
-
-
 function time() {
     current = new Date();
 
